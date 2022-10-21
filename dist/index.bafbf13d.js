d68f3f80da84ca6fbab818892650c880cc2e323c
niranjanrai <rainiranjan1998@gmail.com>
2022-10-22 00:29:52 +0530

    first commit

--- dist/index.bafbf13d.js
+++ dist/index.bafbf13d.js
@@ -568,12 +568,12 @@ class InfoView {
         this.container.appendChild(basicInfo);
         basicInfo.className = "basic_info";
         const icon = document.createElement("img");
-        icon.src = images[data.Weather[0].icon];
+        icon.src = images[data.weather[0].icon];
         icon.className = "w_icon";
         basicInfo.append(icon);
         basicInfo.insertAdjacentHTML("beforeend", `
       <div>
-        <h3>${data.Weather[0].description}</h3>
+        <h3>${data.weather[0].description}</h3>
         <h4> ${data.main.temp} </h4>
       </div>
       `);
